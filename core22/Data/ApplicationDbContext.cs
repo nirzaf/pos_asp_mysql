ba8009dd37095d53ae0f6ada8fa42d9ac9fe9e3b
Farook M Fazrin <nirzaf1@hotmail.com>
2020-07-26 16:21:38 +0530

    Add project files.

---
+++ core22/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using core22.Models.POS.Models;
+
+namespace core22.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Vendor> Vendor { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<PurchaseOrder> PurchaseOrder { get; set; }
+        public DbSet<SalesOrder> SalesOrder { get; set; }
+        public DbSet<GoodsReceive> GoodsReceive { get; set; }
+        public DbSet<InvenTran> InvenTran { get; set; }
+        public DbSet<PurchaseOrderLine> PurchaseOrderLine { get; set; }
+        public DbSet<SalesOrderLine> SalesOrderLine { get; set; }
+        public DbSet<GoodsReceiveLine> GoodsReceiveLine { get; set; }
+    }
+}
